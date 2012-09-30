a833441354c97a26601c5f598746775990a29968
TJ Holowaychuk <tj@vision-media.ca>
2012-09-30 13:56:21 -0700

    add object matching

--- index.js
+++ index.js
@@ -9,7 +9,25 @@
  */
 
 module.exports = function(arr, fn){
+  if ('function' != typeof fn) fn = toFunction(fn);
   for (var i = 0, len = arr.length; i < len; ++i) {
     if (fn(arr[i], i)) return arr[i];
   }
 };
+
+/**
+ * Convert `obj` into a match function.
+ *
+ * @param {Object} obj
+ * @return {Function}
+ * @api private
+ */
+
+function toFunction(obj) {
+  return function(o){
+    for (var key in obj) {
+      if (o[key] != obj[key]) return false;
+    }
+    return true;
+  }
+}
--- test/index.js
+++ test/index.js
@@ -1,16 +1,38 @@
 
-var find = require('..');
+var find = require('..')
+  , assert = require('assert');
+
+var users = [];
+
+users.push({ name: 'Tobi', age: 2, species: 'ferret' })
+users.push({ name: 'Jane', age: 6, species: 'ferret' })
+users.push({ name: 'Luna', age: 2, species: 'cat' })
 
 describe('find(arr, fn)', function(){
   it('should return the first truthy value', function(){
-    var arr = [{ name: 'Tobi' }, { name: 'Jane' }];
-    var ret = find(arr, function(u){ return u.name == 'Jane' });
-    ret.should.equal(arr[1]);
+    var ret = find(users, function(u){ return u.name == 'Jane' });
+    ret.should.equal(users[1]);
   })
 })
 
 describe('find(arr, obj)', function(){
-  it('should return ', function(){
-    
+  describe('with one property', function(){
+    it('should find object with matching property', function(){
+      var ret = find(users, { name: 'Jane' });
+      ret.should.equal(users[1]);
+
+      var ret = find(users, { name: 'Bob' });
+      assert(null == ret);
+    })
+  })
+
+  describe('with multiple properties', function(){
+    it('should find object matching properties', function(){
+      var ret = find(users, { name: 'Jane', age: 6 });
+      ret.should.equal(users[1]);
+
+      var ret = find(users, { name: 'Jane', age: 2 });
+      assert(null == ret);
+    })
   })
 })
